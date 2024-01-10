8757c763d65ca3a401cd1482e272e6a852610b46
kczodape <krunalzodape9@gmail.com>
2024-01-10 16:50:19 +0530

    created dashboard of admin

--- Angular/src/app/modules/admin/admin-routing.module.ts
+++ Angular/src/app/modules/admin/admin-routing.module.ts
@@ -1,12 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BroadbanComponent } from './broadban/broadban.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DthComponent } from './dth/dth.component';
+import { OttComponent } from './ott/ott.component';
+import { SubscribersComponent } from './subscribers/subscribers.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent
-  }
+    component: SidebarComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'broadband',
+        component: BroadbanComponent,
+      },
+      {
+        path: 'dth',
+        component: DthComponent,
+      },
+      {
+        path: 'ott',
+        component: OttComponent,
+      },
+      {
+        path: 'subscribers',
+        component: SubscribersComponent,
+      }
+    ],
+  },
 ];
 
 @NgModule({
--- Angular/src/app/modules/admin/admin.module.ts
+++ Angular/src/app/modules/admin/admin.module.ts
@@ -3,15 +3,47 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { BroadbanComponent } from './broadban/broadban.component';
+import { DthComponent } from './dth/dth.component';
+import { OttComponent } from './ott/ott.component';
+import { SubscribersComponent } from './subscribers/subscribers.component';
+import { ReveneugraphComponent } from './dashboard/reveneugraph/reveneugraph.component';
+import { AgChartsAngularModule } from 'ag-charts-angular';
+import { TotalsubscribercardComponent } from './dashboard/totalsubscribercard/totalsubscribercard.component';
+import { TotalreveneucardComponent } from './dashboard/totalreveneucard/totalreveneucard.component';
+import { BroadbandpiechartComponent } from './dashboard/broadbandpiechart/broadbandpiechart.component';
+import { DthpiechartComponent } from './dashboard/dthpiechart/dthpiechart.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    SidebarComponent,
+    BroadbanComponent,
+    DthComponent,
+    OttComponent,
+    SubscribersComponent,
+    ReveneugraphComponent,
+    TotalsubscribercardComponent,
+    TotalreveneucardComponent,
+    BroadbandpiechartComponent,
+    DthpiechartComponent,
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    AgChartsAngularModule
   ]
 })
 export class AdminModule { }
